d9a7507e23b1cb11526c26ac91779e782ccd1150
JON <jonn.thinkrtc@gmail.com>
2018-09-05 11:23:39 -0400

    implemented BookDbHelper method to create main application database;

--- app/src/main/java/com/udacity/jonathan/bookapp/data/BookDbHelper.java
+++ app/src/main/java/com/udacity/jonathan/bookapp/data/BookDbHelper.java
@@ -1,4 +1,39 @@
 package com.udacity.jonathan.bookapp.data;
 
-public class BookDbHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.udacity.jonathan.bookapp.data.BookContract.BookEntry;
+
+
+public class BookDbHelper extends SQLiteOpenHelper {
+
+    public static final String LOG_TAG = BookDbHelper.class.getSimpleName();
+
+    private static final String DATABASE_NAME = "shelter.db";
+    private static final int DATA_BASE_VERSION = 1;
+
+
+    public BookDbHelper(Context context){
+        super(context, DATABASE_NAME, null, DATA_BASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_BOOKS_TABLE =  "CREATE TABLE " + BookEntry.TABLE_NAME + " ("
+                + BookEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + BookEntry.COLUMN_BOOK_NAME + " TEXT NOT NULL, "
+                + BookEntry.COLUMN_SUPPLIER_NAME + " TEXT, "
+                + BookEntry.COLUMN_SUPPLIER_PHONE + " TEXT NOT NULL, "
+                + BookEntry.COLUMN_BOOK_PRICE + " TEXT NOT NULL, "
+                + BookEntry.COLUMN_BOOK_QUANTITY + " TEXT NOT NULL);";
+
+        // Execute the SQL statement
+        db.execSQL(SQL_CREATE_BOOKS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
