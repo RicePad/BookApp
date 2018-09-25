548462d33a42c5f1cd39ae3d20a5e3ca2b2e0603
JON <jonn.thinkrtc@gmail.com>
2018-09-24 23:40:13 -0400

    updated application design layout with more appealing UI

--- app/src/main/java/com/udacity/jonathan/bookapp/MainActivity.java
+++ app/src/main/java/com/udacity/jonathan/bookapp/MainActivity.java
@@ -1,24 +1,26 @@
 package com.udacity.jonathan.bookapp;
-import com.udacity.jonathan.bookapp.data.BookContract.BookEntry;
-
-
 import android.app.LoaderManager;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.support.design.widget.FloatingActionButton;
+import android.widget.ListView;
 
+
+
+import com.udacity.jonathan.bookapp.data.BookContract.BookEntry;
 import com.udacity.jonathan.bookapp.data.BookCursorAdapter;
 import com.udacity.jonathan.bookapp.data.BookDbHelper;
 
@@ -35,6 +37,16 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Setup FAB to open EditorActivity.
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         // Find the ListView which will be populated with the book data
         ListView bookListView = (ListView) findViewById(R.id.list);
@@ -48,12 +60,31 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         mCursorAdapter = new BookCursorAdapter(this, null);
         bookListView.setAdapter(mCursorAdapter);
 
+        // Setup the item click listener
+        bookListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                // Create new intent to go to {@link EditorActivity}
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+
+                // Form the content URI that represents the specific pet that was clicked on,
+                // by appending the "id" (passed as input to this method) onto the
+                // {@link PetEntry#CONTENT_URI}.
+                // For example, the URI would be "content://com.example.android.books/books/2"
+                // if the book with ID 2 was clicked on.
+                Uri currentBookUri = ContentUris.withAppendedId(BookEntry.CONTENT_URI, id);
+
+                // Set the URI on the data field of the intent
+                intent.setData(currentBookUri);
+
+                // Launch the {@link EditorActivity} to display the data for the current pet.
+                startActivity(intent);
+            }
+        });
+
         // Kick off the loader
         getLoaderManager().initLoader(BOOK_LOADER, null, this);
-
-
     }
-
 
     public void insertBook(){
 
